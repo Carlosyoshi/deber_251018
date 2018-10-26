3abf064da6bffdb87f080c000b83b0f65dda6e1d
Yoshi <Yoshi@YOSHI>
2018-10-26 11:03:53 -0500

    :)

---
+++ src/controlador/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import modelo.Cheaper;
+import modelo.Movie;
+import modelo.Serie;
+
+/**
+ *
+ * @author Yoshi
+ */
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Movie m = new Movie(0, title, genero, creador, 0, 0, true, 0);
+        Serie s = new Serie(0, title, genero, creador, 0, 0, true, 0);
+        Cheaper ch = new Cheaper(0, title, genero, creador, 0, 0, true, 0);
+        
+        */
+    }
+}
